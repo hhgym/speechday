06022afe7fdecf69cb02331d20d8587cb8c34a7e
maphy-psd <psd.phi@gmail.com>
2017-08-09 14:48:55 +0200

    set timeout in PrintElem

--- js/util.js
+++ js/util.js
@@ -19,22 +19,50 @@ function PrintElem(elem, pageTitle) {
     var printWindow = window.open('www.dachsberg.at', pageTitle, '');
     var doc = printWindow.document;
     doc.write("<html><head><title>" + pageTitle + "</title>");
-    doc.write("<link href='css/print.css' rel='stylesheet' type='text/css' media='print' />");
+    doc.write("<link href='css/print.css' rel='stylesheet' type='text/css' media='all' />");
     doc.write("</head><body>");
     doc.write(data);
     doc.write("</body></html>");
     doc.close();
 
-    function show() {
-        if (doc.readyState === "complete") {
-            printWindow.document.close();
-            printWindow.focus();
-            printWindow.print();
-            printWindow.close();
-        } else {
-            setTimeout(show, 100);
-        }
-    }
+    // function show() {
+        // if (doc.readyState === "complete") {
+            // printWindow.document.close();
+            // printWindow.focus();
+            // printWindow.print();
+            // printWindow.close();
+        // } else {
+            // setTimeout(show, 100);
+        // }
+    // }
 
-    show();
+    // show();
+    setTimeout(function(){
+        printWindow.print();
+        printWindow.close();
+        },
+        100
+    );
+    
 }
+
+// Backup print function
+
+// function PrintElem(elem, pageTitle) {
+    // var DocumentContainer = document.getElementById(elem);
+    // var WindowObject = window.open('www.hhgym.de', pageTitle, 'width=750,height=650,top=50,left=50,toolbars=no,scrollbars=yes,status=no,resizable=yes');
+
+    // WindowObject.document.writeln('<!DOCTYPE html>');
+    // WindowObject.document.writeln('<html><head><title></title>');
+    // WindowObject.document.writeln('<link rel="stylesheet" type="text/css" href="css/print.css" media="all">');
+    // WindowObject.document.writeln('</head><body>')
+    
+    // WindowObject.document.writeln(DocumentContainer.innerHTML);
+    // WindowObject.document.writeln('</body></html>');
+
+    // WindowObject.document.close();
+    // WindowObject.focus();
+    // setTimeout(function(){WindowObject.print();WindowObject.close();},100);
+    // WindowObject.print();
+    // WindowObject.close();
+// }
